70ec43f274aaaa12b68aca017bffebed343c1267
elor504 <74613768+elor504@users.noreply.github.com>
2020-12-23 19:48:20 +0200

    walking blend

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -6,11 +6,13 @@ public class AnimatorController : MonoBehaviour
 {
     Player player;
     Animator anim;
-  
+    float velocity = 0.0f;
+    public float acceleration;
+    public float deacceleration;
     // Start is called before the first frame update
     void Start()
     {
-       
+
         player = GetComponent<Player>();
         anim = GetComponent<Animator>();
     }
@@ -18,7 +20,7 @@ public class AnimatorController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     
+
     }
     public void Animate()
     {
@@ -27,14 +29,38 @@ public class AnimatorController : MonoBehaviour
         if (player.isWalking)
         {
             anim.SetBool("isWalking", true);
+            velocity += Time.deltaTime * acceleration;
         }
-        else anim.SetBool("isWalking", false);
+        else
+        {
+            anim.SetBool("isWalking", false);
+            velocity -= Time.deltaTime * deacceleration;
+        }
 
         if (player.dash)
         {
             anim.SetTrigger("dash");
+
         }
-     
+
+
+        //player blend
+        if (velocity > 1)
+        {
+            velocity = 1;
+        }
+        else if (velocity < 0)
+        {
+            velocity = 0;
+        }
+
+        anim.SetFloat("Velocity", velocity);
+
+
+
+
+
+
 
         //Jump Animation//
         if (player.jump)
@@ -50,35 +76,35 @@ public class AnimatorController : MonoBehaviour
         //Attacks Animations//
         for (int i = 0; i < player.Attacks.GetLength(0); i++)
         {
-            if (player.Attacks[i,0]) 
+            if (player.Attacks[i, 0])
             {
                 if (i == 0)
                     anim.SetFloat("attackSpeed", player.stats.attackSpeed);
                 anim.SetTrigger("attack1");
-             if (i==1)
-                anim.SetTrigger("attack2");
-             if (i==2)
-                anim.SetTrigger("attack3");
+                if (i == 1)
+                    anim.SetTrigger("attack2");
+                if (i == 2)
+                    anim.SetTrigger("attack3");
             }
 
         }
-    
+
         //Skills Animations//
         for (int i = 0; i < player.Skills.GetLength(0); i++)
         {
             if (player.Skills[i, 0])
             {
 
-            if (i==0)
-                anim.SetTrigger("skill1");
-            if (i==1)
-                anim.SetTrigger("skill2");
-            if (i==2)
-                anim.SetTrigger("skill3");
-            if (i==3)
-                anim.SetTrigger("skill4");
-            if (i==4)
-                anim.SetTrigger("skill5");
+                if (i == 0)
+                    anim.SetTrigger("skill1");
+                if (i == 1)
+                    anim.SetTrigger("skill2");
+                if (i == 2)
+                    anim.SetTrigger("skill3");
+                if (i == 3)
+                    anim.SetTrigger("skill4");
+                if (i == 4)
+                    anim.SetTrigger("skill5");
             }
         }
 
@@ -109,5 +135,5 @@ public class AnimatorController : MonoBehaviour
         }
 
     }
-   
+
 }

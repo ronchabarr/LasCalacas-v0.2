ccb56ede5015ba681f8a3052bb9cdabfd281c3f3
elor504 <74613768+elor504@users.noreply.github.com>
2021-01-06 17:39:10 +0200

    s

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -23,13 +23,93 @@ public class AnimatorController : MonoBehaviour
     public void UpdateAnimes()
     {
         //Movement Animations//
-        if (player.isWalking)
-        {
-            anim.SetBool("isWalking", true);
-        }
-        else anim.SetBool("isWalking", false);
 
-       
+        if(player.isWalking)
+        {
+            player.StatsSO.velocity += player.StatsSO.acceleration * Time.deltaTime;
+            if (player.StatsSO.velocity > 1)
+            {
+                player.StatsSO.velocity = 1;
+            }
+
+        }
+        else
+        {
+            if (player.StatsSO.velocity > 0)
+            {
+                player.StatsSO.velocity -= player.StatsSO.deacceleration * Time.deltaTime;
+            }
+
+            if (player.StatsSO.velocity < 0)
+            {
+                player.StatsSO.velocity = 0;
+            }
+
+        }
+        if (player.StatsSO.isattackingOne)
+        {
+            player.StatsSO.AtkOne += player.StatsSO.AtkOneacceleration * Time.deltaTime;
+            if (player.StatsSO.AtkOne > 1)
+            {
+                player.StatsSO.AtkOne = 1;
+                player.StatsSO.isattackingOne = false;
+            }
+
+        }
+        else
+        {
+            if (player.StatsSO.AtkOne > 0)
+            {
+                player.StatsSO.AtkOne -= player.StatsSO.AtkOnedeacceleration * Time.deltaTime;
+            }
+
+            if (player.StatsSO.AtkOne < 0)
+            {
+                player.StatsSO.AtkOne = 0;
+            }
+        }
+
+
+        if (player.StatsSO.isattackingTwo)
+        {
+            player.StatsSO.AtkTwo += player.StatsSO.AtkTwoacceleration * Time.deltaTime;
+            if (player.StatsSO.AtkTwo > 1)
+            {
+                player.StatsSO.AtkTwo = 1;
+                player.StatsSO.isattackingTwo = false;
+            }
+
+        }
+        else
+        {
+            if (player.StatsSO.AtkTwo > 0)
+            {
+                player.StatsSO.AtkTwo -= player.StatsSO.AtkTwodeacceleration * Time.deltaTime;
+            }
+
+            if (player.StatsSO.AtkTwo < 0)
+            {
+                player.StatsSO.AtkTwo = 0;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        anim.SetFloat("Velocity", player.StatsSO.velocity);
+        anim.SetFloat("AtkOne", player.StatsSO.AtkOne);
+        anim.SetFloat("AtkTwo", player.StatsSO.AtkTwo);
+
+        anim.SetBool("IsWalking", player.isWalking);
     }
     public void CommandAnimes()
     {
@@ -54,13 +134,19 @@ public class AnimatorController : MonoBehaviour
         //Attacks Animations//
         for (int i = 0; i < player.Attacks.GetLength(0); i++)
         {
-            if (player.Attacks[i,0]) 
+            if (player.Attacks[i, 0])
             {
                 if (i == 0)
-                    anim.SetFloat("attackSpeed",player.stats.attackSpeed);
-                anim.SetTrigger("attack1");
+                { 
+                    anim.SetFloat("attackSpeed", player.stats.attackSpeed);
+                    anim.SetTrigger("attack1");
+                    player.StatsSO.isattackingOne = true;
+                }
              if (i==1)
-                anim.SetTrigger("attack2");
+                {
+                    anim.SetTrigger("attack2");
+                    player.StatsSO.isattackingTwo = true;
+                }
              if (i==2)
                 anim.SetTrigger("attack3");
             }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PlayerStats : ScriptableObject
 {
 
-
+    //READ//
+    //when adding value add transition in Player.PrintData()
     [Header("Speeds:")]
     public float moveSpeed;
     public float DashForce, crouchSpeed, airSpeed, jumpForce;
     public float attackSpeed;
 
     [Header("Attributes:")]
+    public float lifeSteal;
     public int maxHp;
     public int currentHp, stamina, shield, Level;
     public int CritChance;
@@ -27,9 +29,38 @@ public class PlayerStats : ScriptableObject
     [Header("AttacksRanges:")]
     public float basicAttackRange;
 
-    [Header("Abilitys")]
+    [Header("Skill1")]
+
     public float ability1_CD;
+    public float ability1_Duration;
+    public float ability1_Range;
+
+
+
+    [Header("Skill2")]
+
     public float ability2_CD;
+    public float ability2_Duration;
+    public float ability2_Range;
+    public ParticleSystem ability2_Effect;
+
+
+
+    [Header("Skill3")]
+
+    public float ability3_CD;
+    public float ability3_Duration;
+    public float ability3_Range;
+
+
+    [Header("Skill4")]
+
+    public float ability4_CD;
+    public float ability4_Duration;
+    public float ability4_Range;
+
+
+
 
     [Header("Animation related")]
     public float velocity = 0.0f;
@@ -52,10 +83,9 @@ public class PlayerStats : ScriptableObject
     public float SkillTwodeacceleration;
     public bool isUsingSkillTwo = false;
 
-
 }
 
-   
+
 
 
 
